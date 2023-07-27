e39b1e10bc53510bb7207581e630c16d8726ca9c
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-27 13:57:35 -0400

    test process

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Code your solution in this file!
 const companyName = "Scuber"
+// console.log(companyName) /* Testing process */
 
 let mostProfitableNeighborhood = "Chelsea"
+// console.log(mostProfitableNeighborhood) /* Testing process */
 
 let companyCeo = "Susan Smith"
+// console.log(companyCeo) /* Testing process */
